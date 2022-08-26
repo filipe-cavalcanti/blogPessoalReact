7638699047daa12efcd7aee76ba4185a9fb098fb
Filipe Cavalcanti <filipe-cavalcanti@outlook.com>
2022-08-26 14:29:49 -0300

    Navbar, Home e Footer criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/estaticos/navbar/Navbar';
 import Home from './paginas/home/Home';
+import Footer from './components/estaticos/footer/Footer';
 
 function App() {
   return (
-    <Home />
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );
 }
 
