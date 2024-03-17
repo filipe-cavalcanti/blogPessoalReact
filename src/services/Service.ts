f1749bb0374805ba50d4413433e900346dffda6f
Filipe Cavalcanti <102493604+filipe-cavalcanti@users.noreply.github.com>
2024-03-17 03:17:54 -0300

    Update Service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://blogpessoal-536w.onrender.com'
+    baseURL: 'https://blogpessoal-mu7d.onrender.com'
 })
 
 export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
